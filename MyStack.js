b7a7b017cda3161500dcbc3c31875e16ee1efe0e
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-11-25 17:32:39 -0500

    created stack class and tested it

---
+++ MyStack.js
@@ -0,0 +1,46 @@
+module.exports = class MyStack {
+
+	constructor(size) {
+
+		this.stack = new Array(size);
+		this.top = -1;
+	};
+
+	isFull() {
+		return this.top == this.stack.length - 1;
+	}
+
+	isEmpty() {
+		return this.top == -1;
+	}
+
+	print() {
+		return this.stack;
+	}
+
+	push(item) {
+
+		if (this.isFull()) {
+			throw new Error("The stack is full!");
+		}
+		else {
+			this.top ++;
+			this.stack[this.top] = item;
+		}
+
+	}
+
+	pop() {
+
+		if (this.isEmpty()) {
+			throw new Error("The stack is empty!");
+		}
+		else {
+			let top = this.stack[this.top];
+			this.top --;
+			return top;
+		}
+	}
+
+
+}
--- main.js
+++ main.js
@@ -1,48 +1,14 @@
 const MyArray = require('./MyArray.js');
 const LinkedList = require('./LinkedList.js');
+const MyStack = require('./MyStack.js');
 
-let a = new MyArray(5);
-
-for (var i = 0; i < a.length; i ++) {
-	a.insert(i + 1);
+let stack = new MyStack(6)
+for (var i = 0; i < 6; i ++) {
+	stack.push(i + 1);
 }
 
-a.insert(a.length + 1);
-console.log(a);
-a.insertAt(3, 111);
-a.insert(a.length + 1);
-console.log(a);
-
-const l = new LinkedList();
-l.addLast(1)
-l.addLast(2)
-l.addLast(3)
-l.addLast(4)
-l.addFirst(5);
-
-console.log(l.toString());
-
-l.deleteFirst();
-
-console.log(l.toString());
-
-l.deleteLast()
-l.deleteLast()
-l.deleteLast()
-l.deleteFirst();
-
-l.addFirst(1);
-l.addLast(2);
-l.addLast(3);
-l.addLast(4);
-l.reverse();
-
-console.log(l.toString());
-// l.reverse();
-console.log(l.getKthFromTheEnd(1));
-console.log(l.printMiddle());
-
-let b = new LinkedList();
-b.createWithLoop();
-console.log(b.hasLoop());
-
+console.log(stack.print());
+stack.pop();
+stack.pop();
+let p = stack.pop();
+console.log(p);
