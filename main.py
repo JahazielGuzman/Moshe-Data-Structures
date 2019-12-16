4833d6a99df1e53904ee3fdb9a2039b34c5829bd
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-12-16 16:57:15 -0500

    create main.py file

---
+++ main.py
@@ -0,0 +1,12 @@
+from Stack import Stack
+from StackQueue import StackQueue
+from data_structures.PriorityQueue import PriorityQueue
+
+pq = PriorityQueue()
+pq.push(1)
+pq.push(5)
+pq.push(4)
+pq.push(3)
+pq.push(2)
+
+print(pq)
