0bff208e44705ec4c2f8eed2f80e90018f2eb745
JahazielGuzman <jahaziel.guzman93@gmail.com>
2020-01-20 23:04:59 -0500

    redid graph class to use python collections deque class

--- data_structures/Graph.py
+++ data_structures/Graph.py
@@ -1,4 +1,5 @@
 from .LinkedList import LinkedList
+from collections import deque
 
 class GraphNode:
 	def __init__(self, label):
@@ -9,61 +10,69 @@ class GraphNode:
 
 class Graph:
 	def __init__(self):
-		self.adj = {}
-		self.num_nodes = 0
-		self.graph_node_equality_func = lambda graph_node, label: graph_node.label == label
-
-	def __len__(self):
-		return num_nodes
+		self.nodes = {}
+		self.adjacency = {}
 
 	def __str__(self):
 
 		output_str = ""
-		for vertex in self.adj:
-			output_str += f" |{vertex} is connected to nodes: {str(self.adj[vertex])}"
+		for vertex in self.adjacency:
+
+			output_str += f" |{vertex} is connected to nodes: ["
+
+			for node in self.adjacency[vertex]:
+				output_str += f"{node}, "
+
+			output_str += "]\n"
+
 		return output_str
 
 
 
 	def add_node(self, label):
-		if self.adj.get(label):
-			raise LookupError("that node label already exists")
+		if self.adjacency.get(label):
+			return
 
-		self.num_nodes += 1
-		self.adj[label] = LinkedList()
+		self.nodes[label] = GraphNode(label)
+		self.adjacency[label] = deque()
 
 
 
 	def remove_node(self, label):
 
-		self.adj.pop(label)
-		for edge_list in self.adj.values():
-			try:
-				edge_list.remove(label, self.graph_node_equality_func)
-			except IndexError:
-				continue
-			except AttributeError:
-				print("something was None")
+		self.adjacency.pop(label)
 
+		for node in self.adjacency:
+			
+			graph_node = self.nodes[label]
+
+			if graph_node in self.adjacency[node]:
+				self.adjacency[node].remove(graph_node)
+
+
+		self.nodes.pop(label)
 
 
 	def add_edge(self, from_label, to_label):
 		self.both_labels_valid(from_label, to_label)
 
-		self.adj[from_label].addLast(GraphNode(to_label))
+		to_node = self.nodes[to_label]
+		self.adjacency[from_label].append(to_node)
 
 
 
 	def remove_edge(self, from_label, to_label):
 		self.both_labels_valid(from_label, to_label)
 
-		try:
-			self.adj[from_label].remove(to_label, self.graph_node_equality_func)
-		except AttributeError:
-			print(f"Error at {node_label}")
+		to_node = self.nodes[to_label]
+
+		if to_node in self.adjacency[from_label]:
+
+			self.adjacency[from_label].remove(to_node)
 
 
 
 	def both_labels_valid(self, from_label, to_label):
-		if not self.adj.get(from_label) or not self.adj.get(to_label):
-			raise LookupError("that node label does not exist!")
+		if not self.nodes.get(from_label) or not self.nodes.get(to_label):
+			raise LookupError("these node labels do not exist!")
+
